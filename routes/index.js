b4eef9fadef0934764c126437001102a30e8e153
Daniel Santos <danieljorgefilipesantos@gmail.com>
2019-09-12 12:00:48 +0100

    doing the lab

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,25 @@
 
 const { Router } = require('express');
 const router = Router();
+const passport = require('passport');
+const routeGuardMiddleware = require('./../controllers/route-role-guard-middleware');
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+  res.render('index');
 });
+
+
+
+
+router.post('/sign-in', passport.authenticate('sign-in', {
+  successRedirect: "/authentication/private",
+  failureRedirect: "/authentication/sign-in"
+}));
+
+router.get('/private', routeGuardMiddleware, (req, res, next) => {
+  res.render('private');
+});
+
+
 
 module.exports = router;
